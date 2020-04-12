5d88cf5685cef793cc9a1d3d9accec8bc41d34e1
Valentin Chion <valentin.chion@gmail.com>
2020-04-12 14:47:06 +0200

    Add font awesome to project

--- front/src/Main/App.js
+++ front/src/Main/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './App.css';
 import Data from '../Data/Data'; 
 import storageHandler from '../utils/localStorage/storage';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTrashAlt)
 
 class App extends React.Component {
   state = {
